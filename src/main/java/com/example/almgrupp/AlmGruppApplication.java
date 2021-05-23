e5d7f0b8d26eab3dca84c2674174818fd2670cac
kdhedman <kenth.david.hedman@gmail.com>
2021-05-23 12:48:19 +0200

    initial commit

---
+++ src/main/java/com/example/almgrupp/AlmGruppApplication.java
@@ -0,0 +1,13 @@
+package com.example.almgrupp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmGruppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmGruppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/almgrupp/AlmGruppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.almgrupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmGruppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
