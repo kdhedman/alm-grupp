098b89e3ae4f44e874b642904542ee28a136f0ec
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-05-25 10:31:13 +0200

    Created Dragon-model.

---
+++ src/main/java/models/Dragon.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dragon {
+    String name;
+    String size;
+    int age;
+    String challengeRating;
+    String color;
+}
