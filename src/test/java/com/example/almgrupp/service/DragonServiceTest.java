1d1c147d06580e1ff841d861d45dbb6ea9d11bb1
kdhedman <kenth.david.hedman@gmail.com>
2021-05-25 13:48:52 +0200

    added verify to findall

--- src/test/java/com/example/almgrupp/service/DragonServiceTest.java
+++ src/test/java/com/example/almgrupp/service/DragonServiceTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DragonServiceTest {
@@ -37,6 +38,8 @@ class DragonServiceTest {
                 .thenReturn(Arrays.asList(mockDragon));
 
         List<Dragon> actual = dragonService.getDragons();
+
+        verify(mockRepository, times(1)).findAll();
 
         assertEquals(expectedName, actual.get(0).getName());
         assertEquals(expectedSize, actual.get(0).getSize());
